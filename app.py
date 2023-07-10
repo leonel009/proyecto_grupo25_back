041658dbf9c3717cf84685e6e2503b270573bac1
leonel009 <124378386+leonel009@users.noreply.github.com>
2023-07-10 17:34:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Esta es la aplicacion de python
-
 import sqlite3
+from sqlite3 import Error
 
 # Configurar la conexión a la base de datos SQLite
 DATABASE = 'sistema.db'
@@ -11,18 +11,41 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# Crear la tabla 'productos' si no existe
-conn = get_db_connection()
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+def create_table():
+    # Crear la tabla 'Usuarios' si no existe
+    
+    conn = None
+#    try:
+    conn = get_db_connection()
+    print(sqlite3.version)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
         id_u INTEGER PRIMARY KEY,
         correo TEXT NOT NULL,
         nombre TEXT NOT NULL,
-        contra TEXT NOT NULL,)''')
-conn.commit()
-cursor.close()
-conn.close()
+        contra TEXT NOT NULL)''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+#    except Error as e:
+#    print(e)
+#    finally:
+#    if conn:
+#        conn.close()
+    
+    
+    
+    
+    
 
+def create_database():
+    # Verificar si la base de datos existe, si no, crearla y crear la tabla
+    conn = sqlite3.connect(DATABASE)
+    conn.close()
+    create_table()
+
+# Crear la base de datos y la tabla si no existen
+create_database()
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Clases relevantes
@@ -60,10 +83,11 @@ class Sistema():
         self.usuarios = []
         self.conexion = get_db_connection()
         self.cursor = self.conexion.cursor()
+        
        
     def agregar_usuario(self, id_u, correo, nombre, contra ):
         
-        usuario_existente = self.consultar_producto(id_u)
+        usuario_existente = self.consultar_usuario(id_u)
         if usuario_existente:
             print("Ya existe un usuario con ese id.")
             return False
@@ -111,15 +135,17 @@ class Sistema():
         for row in rows:
             id_u, correo, nombre, contra = row
             print(f"Código: {id_u}")
-            print(f"Descripción: {correo}")
-            print(f"Cantidad: {nombre}")
+            print(f"correo: {correo}")
+            print(f"nombre: {nombre}")
             print("-" * 30)
            
     def eliminar_usuario(self, id_u):
-        u = self.consular_usuario(id_u)
-        if u.comprobar_admin():
-            return False
-        self.usuarios.remove(u)
+        self.cursor.execute("DELETE FROM usuarios WHERE id_u = ?", (id_u,))
+        if self.cursor.rowcount > 0:
+            print("Usuario eliminado.")
+            self.conexion.commit()
+        else:
+            print("Usuario no encontrado.")
         
             
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
